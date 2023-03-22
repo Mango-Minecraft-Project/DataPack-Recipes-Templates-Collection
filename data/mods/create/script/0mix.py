4717db9cfaf19acea2fc6a44ddbad162b274466d
Mango <evan940415@gmail.com>
2023-03-22 19:11:08 +0800

    feat: create recipes

---
+++ data/mods/create/script/0mix.py
@@ -0,0 +1,13 @@
+import json
+import pathlib
+bp = "./data/mods/create/recipes/"
+with open("./data/mods/create/script/base.json") as f:
+  bd = json.load(f)
+print(bd)
+for i in pathlib.Path(bp).glob("*.json"):
+  if (i_n:=i.name) not in {"sequenced_assembly.json", "mechanical_crafting"}:
+    with open(bp + i_n) as f:
+      d = json.load(f)
+    print(d, bd | d)
+    with open(bp + i_n, "w") as f:
+      json.dump({"type": i.stem} | bd | d, f, indent=2)
