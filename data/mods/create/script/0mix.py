9193ca4fe3687ec28a62684eff045f1f61dd2627
Mango <evan940415@gmail.com>
2023-03-22 19:32:56 +0800

    fix: type typo

--- data/mods/create/script/0mix.py
+++ data/mods/create/script/0mix.py
@@ -5,9 +5,9 @@ with open("./data/mods/create/script/base.json") as f:
   bd = json.load(f)
 print(bd)
 for i in pathlib.Path(bp).glob("*.json"):
-  if (i_n:=i.name) not in {"sequenced_assembly.json", "mechanical_crafting.json"}:
-    with open(bp + i_n) as f:
+  if i.name not in {"sequenced_assembly.json", "mechanical_crafting.json"}:
+    with open(bp + i.name) as f:
       d = json.load(f)
     print(d, bd | d)
-    with open(bp + i_n, "w") as f:
+    with open(bp + i.name, "w") as f:
       json.dump({"type": f"create:{i.stem}"} | bd | d, f, indent=2)
